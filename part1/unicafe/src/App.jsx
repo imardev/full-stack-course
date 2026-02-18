9e2746802fcbc9163118c5427a7e72f6ab9d1cb1
howl howl <github.casually337@passinbox.com>
2026-02-18 18:37:48 +0000

    exercise 1.7 complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  let total = bad + neutral + good;
+  let score = good * 1 + neutral * 0 + bad * -1;
+  let average = score / total;
+
+  let positive = (good / total) * 100;
+
   const handleClickGood = () => {
     setGood(good + 1);
   };
@@ -27,6 +33,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {total === 0 ? "0" : average}</p>
+      <p>positive {total === 0 ? 0 : positive}</p>
     </div>
   );
 }
