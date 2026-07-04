bc40bfa12d7138d0cb9074c638e368ffce0a58c4
howl howl <github.casually337@passinbox.com>
2026-07-04 20:13:53 +0100

    Finis 2.11

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -19,6 +19,7 @@ morgan.token("body", (req) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
+app.use(express.static("dist"));
 
 let persons = [
   {
