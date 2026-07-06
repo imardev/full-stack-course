975192f8f59ab9fc37320ae0662ec71f0a9d4d57
howl howl <github.casually337@passinbox.com>
2026-07-06 17:12:25 +0100

    Finish 3.16

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -48,10 +48,12 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
@@ -66,7 +68,7 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
@@ -102,6 +104,7 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson);
   });
 });
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
