88892dc1695125662d3bc7f48672f66e94c73632
howl howl <github.casually337@passinbox.com>
2026-07-17 19:53:41 +0100

    Finish 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const blogFavorite = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const blog = blogs.reduce((favorite, current) =>
+    favorite.likes > current.likes ? favorite : current,
+  );
+  return { title: blog.title, author: blog.author, likes: blog.likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  blogFavorite,
 };
