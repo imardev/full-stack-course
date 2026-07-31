bd71e2a9202630f32148bfe1f63da8b2a7a752d5
howl howl <github.casually337@passinbox.com>
2026-07-31 11:56:52 +0100

    Finish 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,17 +1,25 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const assert = require("node:assert/strict");
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  console.log("beforeEach");
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
 
 test("blogs are returned as json", async () => {
   const response = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, 3);
 });
 
 after(async () => {
@@ -24,4 +32,15 @@ test("return blogs with right unique identifier property", async () => {
     assert.ok(blog.id);
     assert.strictEqual(blog._id, undefined);
   });
+});
+
+test("should add one blog that has the right content", async () => {
+  const initialContent = helper.initialBlogs.length;
+  await api
+    .post("/api/blogs")
+    .send(helper.newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const actualContent = await api.get("/api/blogs");
+  assert.strictEqual(actualContent.body.length, initialContent + 1);
 });
