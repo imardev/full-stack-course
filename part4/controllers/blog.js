8b2c4bf3d6680252e811d661ac3259b7388a0262
howl howl <github.casually337@passinbox.com>
2026-07-31 22:48:41 +0100

    Finish 4.19

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -4,29 +4,56 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user");
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
   response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response, next) => {
-  const { title, author, url, likes, userId } = request.body;
-
-  const user = await User.findById(userId);
-
-  if (!title || !url) {
-    return response.status(400).json({ error: "title or url is missing" });
-  }
-
-  const blog = new Blog({
-    title,
-    author,
-    url,
-    likes: likes ?? 0,
-    user: user.id,
-  });
+  const { title, author, url, likes } = request.body;
 
   try {
+    const token = getTokenFrom(request);
+
+    if (!token) {
+      return response.status(401).json({ error: "token missing" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" });
+    }
+
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+      return response.status(401).json({ error: "user not found" });
+    }
+
+    if (!title || !url) {
+      return response.status(400).json({ error: "title or url is missing" });
+    }
+
+    const blog = new Blog({
+      title,
+      author,
+      url,
+      likes: likes ?? 0,
+      user: user.id,
+    });
+
     const savedBlog = await blog.save();
     user.blogs = user.blogs.concat(savedBlog._id);
     await user.save();
