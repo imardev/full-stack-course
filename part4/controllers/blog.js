be120567a277bb180e37f07fef72e54af43d7edb
howl howl <github.casually337@passinbox.com>
2026-07-31 15:43:35 +0100

    Finish 4.13 and 4.14

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,4 +1,5 @@
 const blogRouter = require("express").Router();
+const { response } = require("express");
 const Blog = require("../models/blog");
 
 blogRouter.get("/", (request, response) => {
@@ -13,7 +14,7 @@ blogRouter.post("/", async (request, response, next) => {
   if (!title || !url) {
     return response.status(400).json({ error: "title or url is missing" });
   }
-  
+
   const blog = new Blog({
     title,
     author,
@@ -27,6 +28,28 @@ blogRouter.post("/", async (request, response, next) => {
   } catch (exception) {
     next(exception);
   }
+});
+
+blogRouter.delete("/:id", async (request, response, next) => {
+  const id = request.params.id;
+
+  await Blog.findByIdAndDelete(id);
+
+  return response.status(204).end();
+});
+
+blogRouter.put("/:id", async (request, response, next) => {
+  const id = request.params.id;
+
+  const { likes } = request.body;
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    id,
+    { likes },
+    {
+      new: true,
+    },
+  );
+  response.json(updatedBlog);
 });
 
 module.exports = blogRouter;
