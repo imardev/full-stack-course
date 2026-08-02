9a4fc8b751c8a341e3c60c1b9d0c9cc04d6c9509
howl howl <github.casually337@passinbox.com>
2026-08-02 18:20:32 +0100

    Finish 5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisible = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <>
+      <div style={blogStyle}>
+        <div>
+          <p>
+            {blog.title} {blog.author}
+          </p>
+          <button onClick={toggleVisible}>{visible ? "hide" : "view"}</button>
+        </div>
+        <div style={showWhenVisible} className="details">
+          <p>{blog.url}</p>
+          <div className="likes">
+            {blog.likes} <button>like</button>
+          </div>
+          <p>{blog.user.username}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
