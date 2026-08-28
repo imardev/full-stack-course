34f651895fd57c8768e906595bdbb45bf88260ab
howl howl <github.casually337@passinbox.com>
2026-08-28 13:42:02 +0100

    Finish 3.21

--- part5/frontend/src/components/BlogView.jsx
+++ part5/frontend/src/components/BlogView.jsx
@@ -1,4 +1,10 @@
 import { useParams } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 const BlogView = ({
   blogs,
@@ -18,27 +24,67 @@ const BlogView = ({
     return "Blog no encontrado";
   }
   return (
-    <main>
-      <h1>{blog.title}</h1>
-      <a href={blog.url}>{blog.url}</a>
-      <div className="likes">
-        {blog.likes}
-        {user && (
-          <button
-            onClick={() => {
-              handleLikeBlog(blog);
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h1" component="div" sx={{ fontSize: "1.8rem" }}>
+          {blog.title}
+        </Typography>
+        <Typography sx={{ color: "text.secondary" }}>
+          by {blog.author}
+        </Typography>
+        <Link href={blog.url} variant="body1">
+          {blog.url}
+        </Link>
+        <Typography sx={{ color: "text.secondary" }} variant="body2">
+          Added by {blog.user.username}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+        <div className="likes">
+          {blog.likes} Likes
+          {user && (
+            <Button
+              sx={{
+                color: "blue",
+                borderColor: "blue",
+                ml: 1.5,
+                transition: "all 0.5s ease-in-out",
+                "&:hover": {
+                  backgroundColor: "blue",
+                  color: "white",
+                  borderColor: "blue",
+                },
+              }}
+              variant="outlined"
+              onClick={() => {
+                handleLikeBlog(blog);
+              }}
+            >
+              like
+            </Button>
+          )}
+        </div>
+        {user && blog.user.username === user.username && (
+          <Button
+            onClick={() => handleRemoveBlog(blog)}
+            sx={{
+              color: "red",
+              borderColor: "red",
+              ml: 1.5,
+              transition: "all 0.5s ease-in-out",
+              "&:hover": {
+                backgroundColor: "red",
+                color: "white",
+                borderColor: "red",
+              },
             }}
+            variant="outlined"
           >
-            like
-          </button>
+            remove
+          </Button>
         )}
-      </div>
-
-      <p>Added by {blog.user.username}</p>
-      {user && blog.user.username === user.username && (
-        <button onClick={() => handleRemoveBlog(blog)}>remove</button>
-      )}
-    </main>
+      </CardActions>
+    </Card>
   );
 };
 
