e8cb549691702c6d8ebfb5f11f4ebf73f2d641e4
howl howl <github.casually337@passinbox.com>
2026-09-01 22:59:15 +0100

    Finish 6.7

--- part6/zustand-anecdotes/src/App.jsx
+++ part6/zustand-anecdotes/src/App.jsx
@@ -1,8 +1,14 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
+import { useInitialize } from "./store";
+import { useEffect } from "react";
 
 const App = () => {
+  const { initialize } = useInitialize();
+  useEffect(() => {
+    initialize();
+  }, [initialize]);
   return (
     <div>
       <h2>Anecdotes</h2>
